d4439bdce20af718af8e4bc4ec0103449b5d4909
neelbagora <neelbagora@gmail.com>
2021-10-01 18:29:50 -0400

    Rename classes and implement message handler in db wrapper.

--- backend/functions/classes/gameConfiguration.js
+++ backend/functions/classes/gameConfiguration.js
@@ -6,19 +6,21 @@
  * @param {number} lobbySize the max number of users that can join the lobby.
  * @param {boolean} privateGame game is private if true, public otherwise.
  * @param {Object} lobbyOwner the owner/admin of the game lobby.
+ * @param {boolean} profanityFilter enables the chat profanity filter if true.
  * @return {Object} the game object formed by the parameters.
  */
-function GameSettings(numRounds = 1, roundLength = 60, lobbySize = 1,
-  privateGame = false) {
+function GameConfiguration(numRounds = 1, roundLength = 60, lobbySize = 1,
+  privateGame = false, profanityFilter = false) {
   
   return {
     numRounds: numRounds,
     roundLength: roundLength,
     lobbySize: lobbySize,
     privateGame: privateGame,
+    profanityFilter: profanityFilter,
   };
 }
 
 module.exports = {
-  GameSettings,
+  GameConfiguration,
 }
