69e3eadb5739f9900369462cda74d211ef09ad44
Brendan <brendanjlee5@gmail.com>
2021-10-06 18:45:21 -0400

    Updated createLobby and style

--- frontend/src/pages/createLobby/createLobby.js
+++ frontend/src/pages/createLobby/createLobby.js
@@ -3,32 +3,30 @@ import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 // Components
 import CreateHeader from "../header/header";
+import './createLobby.css'
 
 function CreateLobby() {
   return (
     <div>
+      <CreateHeader />
       <div className='content'>
-      </div>
-      <div className="createLobby">
         <h2>Lobby</h2>
-        <div className="containerSettings">
-
+        <form className='lobby-creation-div'>
           <div className="form-group">
-            <label for="roundNum">Rounds:</label>
-            <select id="roundNum" name="roundNum">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
+              <label for="roundNum">Rounds:</label>
+              <select id="roundNum" name="roundNum">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+          </div> 
           <div className="form-group">
             <label for="drawingTime">Drawing Time:</label>
             <select id="drawingTime" name="drawingTime">
@@ -37,20 +35,19 @@ function CreateLobby() {
               <option value="50">50</option>
               <option value="60">60</option>
               <option value="70">70</option>
+              <option value="80">70</option>
+              <option value="90">70</option>
+              <option value="120">70</option>
+              <option value="180">70</option>
             </select>
           </div>
-
-          <div>
+          <div className='wordlist-container'>
             <h2>Word List Placeholder</h2>
-          </div>
-
-          <div>
-            <Link to='/prestartLobby'>
+          </div> 
+          <Link to='/prestartLobby'>
               <Button varian='primary'>Start Game</Button>{' '}
             </Link>
-          </div>
-
-        </div>
+        </form>
       </div>
     </div>
   );
