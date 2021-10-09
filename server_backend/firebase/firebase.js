6ab60b25f2b742f6118e1e97b5711ccdf1225a1b
neelbagora <neelbagora@gmail.com>
2021-10-09 16:13:20 -0700

    Add server backend functionality

---
+++ server_backend/firebase/firebase.js
@@ -0,0 +1,12 @@
+// Initialize Firebase Admin SDK
+const admin = require("firebase-admin");
+
+// Fetch the service account key JSON file contents
+const serviceAccount = require('../secrets/serviceAccountKey.json');
+const config = require('../config');
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: config.databaseURL,
+});
