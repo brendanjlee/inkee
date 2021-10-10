abf7378066fd4f9e6591047ebd4af6053552a5e7
neelbagora <neelbagora@gmail.com>
2021-10-10 16:43:19 -0700

    Restructure backend

--- server_backend/api/api.js
+++
@@ -1,9 +0,0 @@
-const v1 = require('./v1/v1');
-const express = require('express');
-const router = express.Router();
-
-router.use('/v1', v1);
-
-module.exports = {
-  router,
-}
--- server_backend/api/v1/v1.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/creategame', (req, res) => {
-  const body = req.body;
-});
