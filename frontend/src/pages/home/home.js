d1fddd910f13589a063f5306751018b6a21cd49d
Rishi SHeth <sheth19@purdue.edu>
2021-10-10 11:11:58 -0400

    Welcome page changed to look like figma

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -11,30 +11,28 @@ import tempAvatar from '../../assets/temp_assets/avatarCreationTemp.png'
 function Home() {
   return (
     <div className='root'>
+      <div className='purpleSplat'>
+      <div className='orangeSplat'>
       <div className='header'>
         <img classname='logo' src={Logo} alt='inkee-logo'/>
       </div>
-
-      <div className='content'>
+        <form>
+          <input className='username' type='text' placeholder="enter username"/>
+        </form>
         <div className='avatar-creation'>
           <img className='avatar-img' src={tempAvatar} alt='avatar-placeholder'/>
         </div>
-        <form>
-          <label>Username: </label>
-          <input type='text'/>
-        </form>
-        <div classname='button-div'>
-          <Link to='../createLobby/createLobby.js'>
-            <Button variant="outline-primary" size='lg'>Create Game</Button>{' '}
-          </Link>
+        <div>
           <Link to='../joinLobby/joinLobby.js'>
-            <Button variant="secondary" size='lg'>Join Game</Button>{' '}
+            <Button className='btn' variant="secondary" size='lg'>join game</Button>{' '}
+          </Link>
+        </div>
+        <div>
+          <Link to='../createLobby/createLobby.js'>
+            <Button className='btn' variant="outline-primary" size='lg'>create game</Button>{' '}
           </Link>
         </div>
       </div>
-
-      <div classname='footer'>
-        <p>CS 307 Inkee Team</p>
       </div>
     </div>
   );
