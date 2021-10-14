3e0576d596efaeed6624ff9bd0c9efc0e8185052
neelbagora <neelbagora@gmail.com>
2021-10-14 14:24:07 -0400

    Websocket file implementation

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 require('./startup/routes')(app);
 
+// Set up server for Socket IO.
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-// App Initialization
+// Initalize Socket IO.
+require('./helpers/ws-handler')(io);
+
+// App Initialization.
 const port = process.env.PORT || 3000;
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+server.listen(port, () => console.log(`Listening on port ${port}...`));
 
