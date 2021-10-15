fea8814479d581918039af92ec482de4e1891dae
Rishi SHeth <sheth19@purdue.edu>
2021-10-15 13:34:39 -0400

    Ui updates

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -14,27 +14,27 @@ function Home() {
   return (
     <div className='root'>
       <div className='purpleSplat'>
-      <div className='orangeSplat'>
-      <div className='header'>
-        <img classname='logo' src={Logo} alt='inkee-logo'/>
-      </div>
-        <form>
-          <input className='username' type='text' placeholder="enter username"/>
-        </form>
-        <div align="center">
-        <Canvas></Canvas>
+        <div className='orangeSplat'>
+          <div className='header'>
+        <img className='logo' src={Logo} alt='inkee-logo'/>
         </div>
-        <div>
-          <Link to='../joinLobby/joinLobby.js'>
-            <Button className='btn' variant="secondary" size='lg'>join game</Button>{' '}
-          </Link>
+          <form>
+            <input className='username' type='text' placeholder="enter username..."/>
+          </form>
+          <div align="center">
+            <Canvas></Canvas>
+          </div>
+          <div>
+            <Link to='../joinLobby/joinLobby.js'>
+              <Button className='btn' variant="secondary" size='lg'>join game</Button>{' '}
+            </Link>
+          </div>
+          <div>
+            <Link to='../createLobby/createLobby.js'>
+              <Button className='btn' variant="outline-primary" size='lg'>create game</Button>{' '}
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link to='../createLobby/createLobby.js'>
-            <Button className='btn' variant="outline-primary" size='lg'>create game</Button>{' '}
-          </Link>
-        </div>
-      </div>
       </div>
     </div>
   );
