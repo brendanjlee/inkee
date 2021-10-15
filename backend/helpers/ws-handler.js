00b0297d3343b2fc3831246f9852c89c0958f3a4
neelbagora <neelbagora@gmail.com>
2021-10-14 21:56:04 -0400

    Fix start script

--- backend/helpers/ws-handler.js
+++ backend/helpers/ws-handler.js
@@ -2,9 +2,11 @@ module.exports = (io) => {
   const roomData = {};
 
   io.on('connection', (socket) => {
+    console.log('User joined through socket io.');
     let roomId = null;
 
     io.on('room', (data) => {
+      console.log(`User joined room ${data.socketId} socket io.`);
       roomId = data.socketId;
       socket.join(roomId);
       roomData[data.socketId][socket.id].username = data.username;
