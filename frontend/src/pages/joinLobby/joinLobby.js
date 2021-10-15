87a2b34ee4dc708fc51e33dc41d8a68f93711c1b
Rishi SHeth <sheth19@purdue.edu>
2021-10-15 14:53:58 -0400

    Lobby screen UI updates

--- frontend/src/pages/joinLobby/joinLobby.js
+++ frontend/src/pages/joinLobby/joinLobby.js
@@ -7,22 +7,20 @@ import './joinLobby.css'
 function JoinLobby() {
   return (
     <div className='root'>
-      <CreateHeader />
-      <div className='content'>
-        <h2>Join a Lobby or Random Game</h2>
-        <div className='lobby-form'>
-          <form classNam='form-group'>
-            <label for="game-id">Enter a Game ID:</label>
-            <input type="text" /><br />
+      <div className='greenSplat'>
+        <div className='orangesplat'>
+        <CreateHeader/>
+          <form className='form-group'>
+            <input className='username' type="text" placeholder="enter game ID"/><br />
             <Link to='../prestartLobby/prestartLobby.js'>
               <Button variant='primary'>Join Game</Button>
             </Link>
           </form>
-        </div>
         <Link to='../prestartLobby/prestartLobby.js'>
             <Button variant='primary'>Join Random Game</Button>
         </Link>
-      </div>
+        </div>
+        </div>
     </div>
   );
 }
