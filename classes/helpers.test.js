479718dc79f6b10aa8d1fc331cb9b521072043d0
neelbagora <neelbagora@gmail.com>
2021-10-15 13:09:28 -0400

    Fix unit tests.

--- classes/helpers.test.js
+++ classes/helpers.test.js
@@ -1,10 +1,10 @@
-import { getScore } from "./helpers";
+const helpers = require('./helpers');
 
 test('getScore', () => {
     const now = Date.now() / 1000;
     const later = (Date.now() + 10000) / 1000;
     const roundTime = roundTime / 1000;
-    expect(getScore(now, roundTime)).toBe(Math.floor(((roundTime - elapsedTime) / roundTime) * 500))
+    expect(helpers.getScore(now, roundTime)).toBe(Math.floor(((roundTime - elapsedTime) / roundTime) * 500))
 });
 
 test('populateDisplayTime', () => {
