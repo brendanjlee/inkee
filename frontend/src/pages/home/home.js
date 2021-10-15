b5302bd0390bada23c4a4eca518e5374c4d0ebe8
Rishi SHeth <sheth19@purdue.edu>
2021-10-15 14:53:58 -0400

    Lobby screen UI updates

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { ReactSketchCanvas } from "react-sketch-canvas";
 //Style
 import './home.css'
 // Assets
