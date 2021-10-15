d652701b08203a4de704b739c73466576216de09
neelbagora <neelbagora@gmail.com>
2021-10-15 15:51:08 -0400

    Add files for demo

--- routes/game-handler.js
+++ routes/game-handler.js
@@ -15,46 +15,47 @@ const serializer = new Serializer.Serializer([User, Invite, GameConfiguration]);
 
 /* Create game in Firebase Realtime Database */
 router.post('/', (req, res) => {
-  const gameConfiguration = serializer.deserialize(req.body.gameConfiguration);
-  const userData = serializer.deserialize(req.body.userData);
+  console.log(req.body);
+  const gameConfiguration = req.body.gameConfiguration;
+  const userData = req.body.userData;
   
-  let gameId = null;
+  const inviteCode = new Invite.Invite().inviteCode;
   try {
-    gameId = functions.createGame(gameConfiguration);
+    functions.createGameInstance(gameConfiguration, inviteCode);
   } catch (error) {
     console.log(error);
-  }
-
-  if (gameId === null) {
     res.status(500).send("Error creating game.");
     return;
   }
 
-  let userId = null;
-  try {
-    userId = functions.addNewUser(gameId, userData);
-  } catch (error) {
-    console.log(error);
-  }
+  // let userId = null;
+  // try {
+  //   userId = functions.addNewUser(inviteCode, userData);
+  // } catch (error) {
+  //   console.log(error);
+  //   return;
+  // }
 
-  if (userId === null) {
-    res.status(500).send("Error adding user to the created game.");
-    return;
-  } 
+  // if (userId === null) {
+  //   res.status(500).send("Error adding user to the created game.");
+  //   return;
+  // } 
 
-  let userAdmin = null;
-  try {
-    userAdmin = functions.makeAdmin(userId, gameId);
-  } catch (error) {
-    console.log(error);
-  }
+  // let userAdmin = null;
+  // try {
+  //   userAdmin = functions.makeAdmin(userId, inviteCode);
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).send(`Error making user ${userId} admin of game ${inviteCode}.`);
+  //   return;
+  // }
 
-  if (userAdmin === null) {
-    res.status(500).send("Error making user admin of the created game.");
-    return;
-  }
+  // if (userAdmin === null) {
+  //   res.status(500).send("Error making user admin of the created game.");
+  //   return;
+  // }
 
-  const result = [gameId, userId];
+  const result = inviteCode;
   res.status(200).send(JSON.stringify(result));
 });
 
