b96143b538ec734018ed0b221faa9520aef1152e
neelbagora <neelbagora@gmail.com>
2021-10-20 17:18:29 -0400

    Pull frontend code.

---
+++ frontend/src/Routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import App from "./App";
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' component={App} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
