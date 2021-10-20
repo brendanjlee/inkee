b20877b3d212c76c60814a94cfceaf033e330c07
neelbagora <neelbagora@gmail.com>
2021-10-19 23:41:39 -0400

    Got firebase APIs to work

--- routes/game-handler.js
+++ routes/game-handler.js
@@ -15,45 +15,18 @@ const serializer = new Serializer.Serializer([User, Invite, GameConfiguration]);
 
 /* Create game in Firebase Realtime Database */
 router.post('/', (req, res) => {
-  console.log(req.body);
   const gameConfiguration = req.body.gameConfiguration;
   const userData = req.body.userData;
   
   const inviteCode = new Invite.Invite().inviteCode;
-  try {
-    functions.createGameInstance(gameConfiguration, inviteCode);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error creating game.");
-    return;
-  }
 
-  // let userId = null;
-  // try {
-  //   userId = functions.addNewUser(inviteCode, userData);
-  // } catch (error) {
-  //   console.log(error);
-  //   return;
-  // }
-
-  // if (userId === null) {
-  //   res.status(500).send("Error adding user to the created game.");
-  //   return;
-  // } 
-
-  // let userAdmin = null;
-  // try {
-  //   userAdmin = functions.makeAdmin(userId, inviteCode);
-  // } catch (error) {
-  //   console.log(error);
-  //   res.status(500).send(`Error making user ${userId} admin of game ${inviteCode}.`);
-  //   return;
-  // }
-
-  // if (userAdmin === null) {
-  //   res.status(500).send("Error making user admin of the created game.");
-  //   return;
-  // }
+  functions.createGameInstance(gameConfiguration, inviteCode)
+    .then(() => {
+      functions.addNewUser(inviteCode, userData)
+        .then(() => {
+          functions.makeAdmin(userData, inviteCode);
+        })
+    });
 
   const result = inviteCode;
   res.status(200).send(JSON.stringify(result));
