16243d773c97a7e83f928995dbdb4a0e39a1a10b
Rishi SHeth <sheth19@purdue.edu>
2021-10-23 00:17:18 -0400

    Game page updates

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -5,26 +5,24 @@ import './home.css'
 import Logo from '../../assets/inkee-logo.png'
 import Canvas from '../../components/Canvas';
 
-
-
 function Home() {
-  return (
-    <div className='root'>
-      <iframe width="0" height="0" src="https://www.youtuberepeater.com/watch?v=hQ5PFlYU9sk#gsc.tab=0" frameborder="0" allowfullscreen></iframe>
-      <div className='purpleSplat'>
-        <div className='orangeSplat'>
-          <div className='header'>
-        <img className='logo' src={Logo} alt='inkee-logo'/>
-        </div>
-          <form>
-            <input className='username' type='text' placeholder="enter username..."/>
-          </form>
-          <div align="center">
-            <Canvas></Canvas>
+    return (
+      <div className='root'>
+        <iframe width="0" height="0" src="https://www.youtuberepeater.com/watch?v=hQ5PFlYU9sk#gsc.tab=0" frameborder="0" allowfullscreen></iframe>
+        <div className='purpleSplat'>
+          <div className='orangeSplat'>
+            <div className='header'>
+          <img className='logo' src={Logo} alt='inkee-logo'/>
+          </div>
+            <form>
+              <input className='username' type='text' placeholder="enter username..."/>
+            </form>
+            <div align="center">
+              <Canvas></Canvas>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
-export default Home;
+    );
+  }
+  export default Home;
