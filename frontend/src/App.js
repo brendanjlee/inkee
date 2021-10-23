f54ab817de64e366b47f6572ef32b7b3c0a68e9a
neelbagora <neelbagora@gmail.com>
2021-10-23 15:32:54 -0400

    Add es lint style guide.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router';
+import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './pages/home/home';
 import CreateLobby from './pages/createLobby/createLobby';
@@ -11,43 +10,19 @@ import Game from './pages/game/game';
 import GameDrawer from './pages/game/gameDrawer';
 import GameGuesser from './pages/game/gameGuesser';
 import testPage from './reactTesting/testPage';
-import io from 'socket.io-client';
+
 
 function App() {
-  const [socket, setSocket] = useState(null);
-  const history = useHistory();
-
-  useEffect(() => {
-    // Initialize Socket connection.
-    const newSocket = io(`http://${window.location.hostname}:3001`, {
-      transports: ['websocket', 'polling'],
-      upgrade: true,
-    });
-    
-    const reconnect = () => {
-      newSocket.io.opts.transports = ["polling", "websocket"];
-    };
-
-    newSocket.on("connect_error", reconnect);
-    setSocket(newSocket);
-
-    // Clean-up routine for socket.
-    return () => {
-      newSocket.close();
-      newSocket.off("connect_error", reconnect);
-    }
-  }, [setSocket]);
-
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path='/' exact render={(props) => (<Home socket={socket} history={history} {...props} />)}/>
-          <Route path='/create' render={(props) => (<CreateLobby socket={socket} history={history} {...props} />)}/>
-          <Route path='/join' render={(props) => (<JoinLobby socket={socket} history={history} {...props} />)}/>
-          <Route path='/prestart' render={(props) => (<PrestartLobby socket={socket} history={history} {...props} />)}/>
-          <Route path='/setup' render={(props) => (<SetupProfile socket={socket} history={history} {...props} />)}/>
-          <Route path='/game' render={(props) => (<Game socket={socket} history={history} {...props} />)}/>
+          <Route path='/' exact component={Home}/>
+          <Route path='/createLobby' component={CreateLobby}/>
+          <Route path='/joinLobby' component={JoinLobby}/>
+          <Route path='/prestartLobby' component={PrestartLobby}/>
+          <Route path='/setupProfile' component={SetupProfile}/>
+          <Route path='/game' component={Game}/>
           <Route path='/game/gameDrawer' component={GameDrawer}/>
           <Route path='/game/gameGuesser' component={GameGuesser}/>
           <Route path='/testPage' component={testPage} />
--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,71 +1,30 @@
+import React from 'react'
+//Style
 import './home.css'
-import React, { useRef, useState } from 'react'
+// Assets
 import Logo from '../../assets/inkee-logo.png'
 import Canvas from '../../components/Canvas';
-import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
-function Home({socket, history}) {
-  const canvas = useRef();
-  const [userName, setUsername] = useState('');
-  
-  const handleSubmit = (route) => {
-    // TODO: Upload image to the backend server and retrieve the URL.
-    // Push to the next page based on route.
-    history.push({
-      pathname: route,
-      state: {
-        username: userName,
-        avatar: 'temp_avatar',
-      }
-    });
 
-    
-    canvas.current.exportImage("png")
-      .then(data => {
-        console.log(data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
 
+function Home() {
   return (
     <div className='root'>
+      <iframe width="0" height="0" src="https://www.youtuberepeater.com/watch?v=hQ5PFlYU9sk#gsc.tab=0" frameborder="0" allowfullscreen></iframe>
       <div className='purpleSplat'>
         <div className='orangeSplat'>
           <div className='header'>
         <img className='logo' src={Logo} alt='inkee-logo'/>
         </div>
-          <form onChange={setUsername}>
+          <form>
             <input className='username' type='text' placeholder="enter username..."/>
           </form>
           <div align="center">
-            <Canvas canvas={canvas}></Canvas>
-            <div>
-              <Button
-                onClick={() => handleSubmit('/join')}
-                className='btn'
-                variant="secondary"
-                size='lg'
-              >
-                join game
-              </Button>{' '}
-            </div>
-            <div>
-              <Button
-                onClick={() => handleSubmit('/create')}
-                className='btn'
-                variant="outline-primary"
-                size='lg'
-              >
-                create game
-              </Button>{' '}
-            </div>
+            <Canvas></Canvas>
           </div>
         </div>
       </div>
     </div>
-    );
-  }
-  export default Home;
+  );
+}
+export default Home;
