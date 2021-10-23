1e272f4685410ece7468f39234c6ac41a108e68b
Brendan <brendanjlee5@gmail.com>
2021-10-23 10:35:22 -0400

    Update BootstrapTextarea.js

--- frontend/src/components/BootstrapTextarea.js
+++ frontend/src/components/BootstrapTextarea.js
@@ -10,6 +10,12 @@ class BootstrapTextarea extends React.Component{
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.hanldeFormSubmit = this.hanldeFormSubmit.bind(this);
+    }
+
+    hanldeFormSubmit(event) {
+        event.preventDefault();
+        console.log(event.target[0].value);
     }
 
     handleInputChange(event) {
@@ -18,13 +24,14 @@ class BootstrapTextarea extends React.Component{
         });
 
         var textInput = document.getElementById('')
-        //console.warn(this.state)
+        //console.log('textevent')
+        console.warn(this.state)
     }
 
     render(){
         return(
             <div>
-                <Form>
+                <Form onSubmit={this.hanldeFormSubmit}>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Example textarea</Form.Label>
                         <Form.Control as="textarea" rows="3" name="address" onChange={this.handleInputChange} />
