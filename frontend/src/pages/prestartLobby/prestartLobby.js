0243ab831a4153a914aa38a16c0b8e4cbaadd437
William Gerber <willgerber01@gmail.com>
2021-10-24 15:21:56 -0400

    Invite chagnes

--- frontend/src/pages/prestartLobby/prestartLobby.js
+++ frontend/src/pages/prestartLobby/prestartLobby.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import CreateHeader from "../header/header";
-import { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import CreateHeader from "../../components/header/header";
 import './prestartLobby.css'
 
 function PrestartLobby({socket, history}) {
