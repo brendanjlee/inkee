b237aec15463d6496a8e5e765bf813c28acd192a
William Gerber <willgerber01@gmail.com>
2021-10-28 16:01:03 -0400

    Add chat message feature

--- frontend/src/pages/game/game.js
+++ frontend/src/pages/game/game.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+
 //Style
 import './game.css'
 // Assets
 import DrawArea from '../../components/DrawArea';
 
-function Game() {
+function Game({socket, history}) {
+
+  // useEffect(() => {
+  //   const sendMessage = document.querySelector('#sendMessage');
+    
+  //   sendMessage.addEventListener('submit', function (e) {
+  //     e.preventDefault();
+  //     const message = this.firstElementChild.value;
+  //     this.firstElementChild.value = '';
+  //     socket.emit('message', { message });
+  //   });
+  // }, []);
+
+  socket.on('message', writeMessage);
   return (
     <div className='gameRoot'>
       <div className='purpleSplatTwo'>
@@ -19,7 +33,7 @@ function Game() {
               <DrawArea></DrawArea>
               <div className="chat">chat</div>
             </div>
-            <div className="bottomContainer">
+            <div className="sendMessage">
               <input type='text' placeholder="enter guess..."/>
             </div>
           </div>
@@ -28,4 +42,22 @@ function Game() {
     </div>
   );
 }
+
+function writeMessage({ name = '', message, id }) {
+  const p = document.createElement('p');
+  const chatBox = document.createTextNode(`${message}`);
+  const messages = document.querySelector('.chat');
+  if (name !== '') {
+    const span = document.createElement('span');
+    span.textContent = `${name}: `;
+    span.classList.add('fw-bold');
+    p.append(span);
+  }
+  p.classList.add('p-2', 'mb-0');
+  p.append(chatBox);
+  messages.appendChild(p);
+  messages.scrollTop = messages.scrollHeight;
+}
+
+
 export default Game
