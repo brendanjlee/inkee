c6d3a079800bead2503617cb7630006065ab2247
Brendan <brendanjlee5@gmail.com>
2021-11-01 23:02:34 -0400

    Updated createLobby (parse content)

--- frontend/src/components/CSVReader2.js
+++ frontend/src/components/CSVReader2.js
@@ -1,42 +1,69 @@
 import { CSVReader } from "react-papaparse"
-import React from "react"
+import React, { Component } from "react"
 import { useState } from "react"
 
-function CSVReader2() {
-  const handleOnDrop = (data) => {
+class CSVReader2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {wordList: []};
+  }
+
+  updateWordlist = (words) => {
+    if (words) {
+      this.setState({wordList: words});
+      console.log('wordList state updated');
+    }
+    else {
+      this.setState({wordList: null});
+    }
+    console.log(`wordList: ${this.state.wordList}`)
+  }
+
+  deleteWordList = () => {
+    this.setState({wordList: []});
+    console.log('wordList state updated');
+  }
+
+  handleOnDrop = (data) => {
     console.log('---------------------------');
-    console.log(`Data Len: ${data.length}`)
-    let words = [];
+    console.log(`handleOnDrop`);
+    
+    // parse data
+    let words = [];  // holder for words
     for (let i = 0; i < data.length; i++) {
       let word = data[i].data;  // array object ['word']
       if (!words.includes(word[0])) words.push(word[0]);
     }
-    console.log(`Words from CSV: ${words}`);
+
+    // Update state 
+    this.updateWordlist(words);
   };
 
-  const handleOnError = (err, file, inputElem, reason) => {
+  handleOnError = (err, file, inputElem, reason) => {
     console.log(err);
   };
 
-  const handleOnRemoveFile = (data) => {
+  handleOnRemoveFile = (data) => {
     console.log('---------------------------');
-    console.log(data);
-    console.log('---------------------------');
+    console.log('handleOnRemoveFile');
+    this.updateWordlist();
   };
 
-  return (
-    <>
-      <h5>Or Upload a CSV File</h5>
-      <CSVReader
-        onDrop={handleOnDrop}
-        onError={handleOnError}
-        addRemoveButton
-        onRemoveFile={handleOnRemoveFile}
-      >
-        <span>Drop CSV file here or click to upload.</span>
-      </CSVReader>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <h5>Or Upload a CSV File</h5>
+        <CSVReader
+          onDrop={this.handleOnDrop}
+          onError={this.handleOnError}
+          addRemoveButton
+          onRemoveFile={this.handleOnRemoveFile}
+        >
+          <span>Drop CSV file here or click to upload.</span>
+        </CSVReader>
+      </>
+    );
+  }
 }
 
-export default CSVReader2
+export default CSVReader2;
