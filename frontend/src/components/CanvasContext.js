eec46e3a55ce59c7327529dfc08fc02f55ff4684
brendanjlee <brendanjlee5@gmail.com>
2021-11-03 14:51:38 -0400

    Update CanvasContext.js
    emits stroke line width change event

--- frontend/src/components/CanvasContext.js
+++ frontend/src/components/CanvasContext.js
@@ -86,6 +86,9 @@ export const CanvasProvider = ({ children, socket = null }) => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
     context.lineWidth = lineWidthValue;
+    if (socket) {
+      socket.emit('lineWidthChange', lineWidthValue);
+    }
   }
 
   const exportImage = () => {
