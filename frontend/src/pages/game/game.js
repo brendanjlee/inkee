2303624afe2a66b8d9f12aad51b391d59b90f0e5
William Gerber <willgerber01@gmail.com>
2021-11-03 21:17:28 -0400

    Guessing Algorihtm Addition

--- frontend/src/pages/game/game.js
+++ frontend/src/pages/game/game.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import leven from 'leven';
 //Style
 import './game.css'
 // Assets
@@ -20,15 +21,44 @@ function Game({socket, history}) {
 
   useEffect(() => {
     const sendMessage = document.querySelector('#sendMessage');
-    
+    const wordChoice = "TestWord";
     sendMessage.addEventListener('keypress', function (e) {
       if (e.key === 'Enter') {
         e.preventDefault();
         const message = this.firstElementChild.value;
+        const distance = leven(wordChoice, message);
         this.firstElementChild.value = '';
-        socket.emit('chatMessage', { message });
+        if (distance === 0) {
+          socket.emit('correctGuess', { message: 'Wow you are kinda smart!!!'});
+        }
+        else if (distance < 3) {
+          socket.emit('closeGuess', { message: 'So close keep trying!!' });
+          socket.emit('chatMessage', { message })
+        }
+        else {
+          socket.emit('chatMessage', { message });
+        }
+        
       }
     });
+    socket.on('closeGuess', (data) => {
+      console.log(data);
+      setMessages([...messages, data]);
+      writeMessage({
+        name: data.uid,
+        message: 'So close keep trying!!'
+       }, { closeGuess: true });
+    });
+
+    socket.on('correctGuess', (data) => {
+      console.log(data);
+      setMessages(['Wow you are kinda smart!!!', data]);
+      writeMessage({
+        name: data.uid,
+        message: 'Wow you are kinda smart!!!'
+       }, { correctGuess: true });
+    });
+
 
     socket.on('chatMessage', (data) => {
       console.log(data);
@@ -51,7 +81,7 @@ function Game({socket, history}) {
           <div className='limeSplat'>
             <div className='inkeeLogo'>
               <div className="topContainer" >
-                <div className="word" >word</div>
+                <div className="word" >TestWord</div>
                 <div className="time" > 3:19 </div>
               </div>
               <div className="middleContainer">
@@ -77,7 +107,7 @@ function Game({socket, history}) {
   );
 }
 
-function writeMessage({ name = '', message}) {
+function writeMessage({ name = '', message}, {correctGuess = false, closeGuess = false} = {}) {
   const p = document.createElement('p');
   const chatBox = document.createTextNode(`${message}`);
   const messages = document.getElementById('chat');
@@ -88,6 +118,13 @@ function writeMessage({ name = '', message}) {
     p.append(span);
   }
   p.classList.add('p-2', 'mb-0');
+  if (closeGuess) {
+    p.classList.add('closeAnswer');
+  }
+
+  if (correctGuess) {
+    p.classList.add('correctAnswer');
+  }
   p.append(chatBox);
   messages.appendChild(p);
   messages.scrollTop = messages.scrollHeight;
