2bab4937b35676c06a67912f91c6e0564d12e5b9
brendanjlee <brendanjlee5@gmail.com>
2021-12-02 17:39:28 -0500

    Fixed Undo/Redo Button

--- frontend/src/components/UndoStroke.js
+++ frontend/src/components/UndoStroke.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { useCanvas } from "./CanvasContext";
+import React from 'react';
+import { useCanvas } from './CanvasContext';
 
 export const UndoStrokeButton = () => {
-  const { undoStroke} = useCanvas();
+  const { undo } = useCanvas();
 
-  return <button className='btn2' onClick={undoStroke}>undo</button>;
+  return <button className='btn2' onClick={undo}>undo</button>;
 };
