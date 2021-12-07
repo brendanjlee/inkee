d6b331ac09a64cea9daa2cc770365ad0f04b7969
neelbagora <neelbagora@gmail.com>
2021-12-07 17:40:16 -0500

    Fix issue with score not rounding

--- backend/controllers/game.js
+++ backend/controllers/game.js
@@ -255,7 +255,7 @@ class Game {
     const primaryDrawer = rooms[this.socket.roomId].users[userIds[primaryDrawerIdx]];
     
     if (primaryDrawer) {
-      rooms[this.socket.roomId].users[primaryDrawer.uid].score += drawingScore;
+      rooms[this.socket.roomId].users[primaryDrawer.uid].score += Math.floor(drawingScore);
       this.io.to(this.socket.roomId).emit('scoreUpdate');
     }
 
@@ -293,7 +293,7 @@ class Game {
         userRanks.push(prepareUser(rooms[this.socket.roomId].users[userName]));
       });
       
-      userRanks.sort((userA, userB) => (userA.score > userB.score) ? 1 : -1);
+      userRanks.sort((userA, userB) => (userA.score < userB.score) ? 1 : -1);
       this.io.to(this.socket.roomId).emit('endGame', userRanks);
       
       if (rooms[this.socket.roomId]) {
