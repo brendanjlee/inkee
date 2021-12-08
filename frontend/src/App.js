f741d219cf683314622b6641baea67a7c6876b06
neelbagora <neelbagora@gmail.com>
2021-12-08 13:46:00 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import { PlaySound } from './components/PlaySound';
 function App() {
   const [socket, setSocket] = useState(null);
   const history = useHistory();
+  console.log(randomVar);
   
   useEffect(() => {
     // Initialize Socket connection.
