c9dc2a1b5401a9330c555c52c2434f2437f972be
neelbagora <neelbagora@gmail.com>
2021-12-08 19:18:22 -0500

    Delete useless stuff

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,6 @@
-const databaseURL = 'wss://splat-io-default-rtdb.firebaseio.com/';
-const storageBucket = 'splat-io.appspot.com';
-/* eslint-disable-next-line */
 const adminPassword = process.env.PASSWORD ||
   '$2b$10$heqvAkYMez.Va6Et2uXInOnkCT6/uQj1brkrbyG3LpopDklcq7ZOS';
 
 module.exports = {
-  databaseURL,
-  storageBucket,
   adminPassword,
 };
--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const imageRouter = require('../routes/image-uploader');
 
 const corsOptions = {
   origin: ['https://admin.socket.io', 'https://inkee.io', 'https://inkee-io.herokuapp.com'],
@@ -13,5 +12,4 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(cookieParser());
   app.use(cors(corsOptions));
-  app.use('/avatar', imageRouter.router);
 };
